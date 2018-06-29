b378ac02b8683e001bc3d07bbafa2de979bf275e
Sebastian Lozada <fslc92@gmail.com>
2018-06-29 12:03:18 -0500

    added new funcition addMessage

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,27 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
+exports.addMessage = functions.https.onRequest((request, response) => __awaiter(this, void 0, void 0, function* () {
+    const original = request.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    const snapshot = yield admin.database().ref('/messages').push({ original: original });
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return response.redirect(303, snapshot.ref.toString());
+}));
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +9,12 @@ import * as functions from 'firebase-functions';
  export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
  });
+
+
+export const addMessage = functions.https.onRequest(async (request, response) => {
+ const original = request.query.text;
+ // Push the new message into the Realtime Database using the Firebase Admin SDK.
+ const snapshot = await admin.database().ref('/messages').push({original: original})
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  return response.redirect(303, snapshot.ref.toString());
+});
